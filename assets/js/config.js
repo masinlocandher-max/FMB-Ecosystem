06e3c292202925546718743f7b3ced65401666d7
masinlocandher-max <masinlocandher@gmail.com>
2026-07-12 03:54:11 +0800

    Make public configuration preview-safe without exposing secrets

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,9 @@
-window.FMB_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
-  AUTH_REDIRECT_URL: "https://www.francinemariebautista.com/member.html"
-};
+(function(){
+  const base=new URL('./',window.location.href).href;
+  window.FMB_CONFIG={
+    SUPABASE_URL:'',
+    SUPABASE_ANON_KEY:'',
+    SITE_URL:base,
+    AUTH_REDIRECT_URL:new URL('member.html',base).href
+  };
+})();
