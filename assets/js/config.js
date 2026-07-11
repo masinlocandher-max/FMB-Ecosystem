2c17e7e92a81b800643c6d25ddaba5db9ffac937
masinlocandher-max <masinlocandher@gmail.com>
2026-07-12 05:40:26 +0800

    Connect Supabase authentication configuration

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,8 @@
 (function(){
   const base=new URL('./',window.location.href).href;
   window.FMB_CONFIG={
-    SUPABASE_URL:'',
-    SUPABASE_ANON_KEY:'',
+    SUPABASE_URL:'https://wjnavdpppnhxbuydkrkd.supabase.co',
+    SUPABASE_ANON_KEY:'sb_publishable_bpdFntTHbHmxsG4L0PtcCw_5dJ8gpr8',
     SITE_URL:base,
     AUTH_REDIRECT_URL:new URL('member.html',base).href
   };
