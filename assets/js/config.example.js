68af8d1000ab1cc220c01105194fd8eb286477a7
masinlocandher-max <masinlocandher@gmail.com>
2026-07-11 08:51:05 +0800

    Add example member service configuration

---
+++ assets/js/config.example.js
@@ -0,0 +1,5 @@
+window.FMB_CONFIG = {
+  SUPABASE_URL: "https://YOUR_PROJECT.supabase.co",
+  SUPABASE_ANON_KEY: "YOUR_PUBLIC_ANON_KEY",
+  AUTH_REDIRECT_URL: "https://www.francinemariebautista.com/member.html"
+};
